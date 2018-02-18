7dfc0bd917749760aefd163eccc80f8148f5958c
mrconter1 <32551374+mrconter1@users.noreply.github.com>
2018-02-18 15:38:07 +0100

    Add files via upload

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -1,16 +1,25 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class DataHandler {
 
+	/* --------------------------------------------------------------------- */
+	/* Following code is to extract both accelerometer 
+	 * data from headset and the GSR. The "Calculate Offset" 
+	 * section */
+
+	//Headset accelermeter data extraction
 	public List<Double> accelerometerMagnitude(List<String> time, List<String> x, List<String> y, List<String> z, double conversionFactor) { //headset
 		
 		List<Double> accMag = new ArrayList<Double>();
 		
+		//Using time to put the total magnitute data in the right place. 
 		double xAcc, yAcc, zAcc, totalAcc;
 		for (int i = 1; i < x.size(); i++) {
 			
@@ -43,10 +52,12 @@ public class DataHandler {
 		
 	}
 	
-public List<Double> accelerometerMagnitude(List<String> x, List<String> y, List<String> z, int sampleRate, double conversionFactor) { //gsr
+	//GSR extraction of accelerometer data
+	public List<Double> accelerometerMagnitude(List<String> x, List<String> y, List<String> z, int sampleRate, double conversionFactor) { //gsr
 		
 		List<Double> accMag = new ArrayList<Double>();
 		
+		//Same as with the headset only that you have to multiply the acceleration with 9.81/64
 		double xAcc, yAcc, zAcc, totalAcc;
 		double pos = 0;
 		int oldArrPos, arrPos;
@@ -78,145 +89,52 @@ public List<Double> accelerometerMagnitude(List<String> x, List<String> y, List<
 		return accMag;
 	}
 	
-	public List<Double> accMag(File file, int accStartColumn) throws FileNotFoundException, IOException { //headset
+	//First function called when handling headset data. -1 is for clarifying that it is headset data
+	public List<Double> accMag(File file, int accStartColumn) throws FileNotFoundException, IOException { 
 		
 		return accMag(file, accStartColumn, -1);
 	}
 	
-	public List<Double> accMag(File file, int accStartColumn, int sampleRate) throws FileNotFoundException, IOException { //gsr
+	//This function handles both headset and GSR data. 
+	public List<Double> accMag(File file, int accStartColumn, int sampleRate) throws FileNotFoundException, IOException { 
 		
 		List<String> time = null;
 		List<Double> accelerationMag;
-		
+
 		if(sampleRate == -1){
 			time = (new FileHandler()).getColumn(file, 3);
 		}
 		
-		List<String> x = (new FileHandler()).getColumn(file, accStartColumn);		
+		List<String> x = (new FileHandler()).getColumn(file, accStartColumn);
 		List<String> y = (new FileHandler()).getColumn(file, accStartColumn + 1);
 		List<String> z = (new FileHandler()).getColumn(file, accStartColumn + 2);
 		
-		if(sampleRate > -1) // gsr
+		//First case is for GSR and second for Headset
+		if(sampleRate > -1) {
 			accelerationMag = accelerometerMagnitude(x,y,z, sampleRate, (9.81/64));
-		else 				// headset
+		} else {
 			accelerationMag = accelerometerMagnitude(time, x,y,z, 1);
+		}
 		
 		return accelerationMag;
 		
 	}
 	
-	public double calcOffset(List<Double> head, List<Double> hand) throws FileNotFoundException, IOException {
-		
-		double headThreshold = 0.7;
-		double handThreshold = 0.7;
-		
-		double headAverage = average(head, 0, head.size() - 1);
-		//System.out.println("head.size: " + head.size());
-		//System.out.println("Average head: " + headAverage);
-		double handAverage = average(hand, 0, hand.size() - 1);
-		//System.out.println("Average hand: " + handAverage);
-		
-		int pos = 0;
-		int searchRange = 500;
-		int still = 0;
-		double sens = 0.15;
-		int stillCount = 0;
-		
-		while (pos < hand.size()) {
-			
-			double avg = 0;
-			int count = 0;
-			double largest = 0;
-			for (int i = (pos - searchRange); i < (pos + searchRange); i++) {
-				if (i >= 0 && i < hand.size()) {
-					if (hand.get(i) != null) {
-						//avg += Math.abs(headAverage-head.get(i));
-						//count++;
-						double tempDiff = Math.abs(handAverage-hand.get(i));
-						if (tempDiff > largest) largest = tempDiff;
-					}
-				}
-			}
-			//avg/=count;
-			
-			double diff = largest;
-			//double diff = Math.abs(headAverage-avg);
-			if (diff < sens && still == 0) {
-				still = 1;
-				stillCount++;
-				System.out.println(pos);
-				System.out.println("stillcount: " + stillCount);
-			} 
-			if (diff > sens && still == 1) {
-				still = 0;
-				//System.out.println(pos);
-				//System.out.println("move");
-			}
-			
-			pos++;
-			
-		}
-		
-		return 0.0;
-		
-		//In milliseconds
-		/*int searchRange = 10;
-		
-		double averageValHead = 0;
-		int headPos = searchRange;
-		while (averageValHead < headThreshold && headPos < 1000*10) {
-			
-			double sum = 0;
-			int count = 0;
-			for (int i = (headPos - searchRange); i < (headPos + searchRange); i++) {
-				
-				if (head.get(i) != null) {
-					sum += Math.abs(head.get(i) - headAverage);
-					count++;
-				}
-				
-			}
-			
-			averageValHead = sum/((double) count);
-			headPos++;
-			System.out.println("sum: " + sum);
-			System.out.println("averageValHead: " + averageValHead);
-			System.out.println("Headpos: " + headPos);
-			
-		}
-		
-		double averageValHand = 0;
-		int handPos = searchRange;
-		while (averageValHand < handThreshold && handPos < 1000*10) {
-			
-			double sum = 0;
-			int count = 0;
-			for (int i = (handPos - searchRange); i < (handPos + searchRange); i++) {
-				
-				if (hand.get(i) != null) {
-					sum += Math.abs(hand.get(i) - handAverage);
-					count++;
-				}
-				
-			}
-			
-			averageValHand = sum/count;
-			handPos++;
-			
-		}
-		
-		//Returns hand offset compared to headpos
-		return handPos - headPos;*/
-		
-	}
+	/* End of "Calculate Offset" section */
+	/* --------------------------------------------------------------------- */
 	
+	/* --------------------------------------------------------------------- */
+	/* This section contains general data extraction functions */
+	
+	//Returns average value between two pivots (indexes)
 	public double average(List<Double> data, int a, int b) throws FileNotFoundException, IOException {
 		
 		double sum = 0;
 		int count = 0;
 		for(int i = a; i < b; i++) {
 			
-			if (data.get(i) != null) {
+			//Only reads valid data
+			if (i >= 0 && i < data.size() && data.get(i) != null) {
 				sum += data.get(i);
 				count++;
 			}
@@ -227,6 +145,185 @@ public List<Double> accelerometerMagnitude(List<String> x, List<String> y, List<
 		
 	}
 	
+	//Return value of max element between two pivots
+	public double maxVal(List<Double> data, int a, int b) throws FileNotFoundException, IOException {
+		
+		int count = 0;
+		double max = 0;
+		for(int i = a; i < b; i++) {
+			
+			if (i >= 0 && i < data.size() && data.get(i) != null && data.get(i) > max) {
+				max = data.get(i);
+				count ++;
+			}
+			
+		}
+		
+		return max;
+		
+	}
+	
+	//Return value of min element between two pivots
+	public double minVal(List<Double> data, int a, int b) throws FileNotFoundException, IOException {
+		
+		double sum = 0;
+		int count = 0;
+		double min = 100000;
+		for(int i = a; i < b; i++) {
+			
+			if (i >= 0 && i < data.size() && data.get(i) != null && data.get(i) < min) {
+				min = data.get(i);
+				count++;
+			}
+			
+		}
+		
+		return min;
+		
+	}
+	
+	//Returns stdev value between two pivots
+	public double stdev(List<Double> data, int a, int b) throws FileNotFoundException, IOException {
+		
+		double stdev = 0;
+		
+		double avg = average(data, a, b);
+		int dataPoints = numberOfDatapoints(data, a, b);
+		
+		//Calculate sum of |x - avg|^2
+		double distSum = 0;
+		for(int i = a; i < b; i++) {
+			if (i >= 0 && i < data.size() && data.get(i) != null) {
+				distSum += Math.pow(Math.abs(data.get(i) - avg), 2);
+			}
+		}
+		
+		//Divides distSum by (n - 1) or n if value exists and takes the square root out of the result
+		if (dataPoints > 1) {
+			stdev = Math.sqrt(distSum/(dataPoints - 1));
+		} else if (dataPoints == 1) {
+			stdev = Math.sqrt(distSum);
+		} else {
+			stdev = 0.0;
+		}
+		
+		return stdev;
+		
+	}
+	
+	//Returns skewness from sample between two pivots using this formula:
+	//http://www.macroption.com/images/formulas/sample-skewness-formula-standard-deviation.png
+	public double skewness(List<Double> data, int a, int b) throws FileNotFoundException, IOException {
+		
+		double skewness = 0;
+		double avg = average(data, a, b);
+		int dataPoints = numberOfDatapoints(data, a, b);
+		
+		//Calculates n/((n-1)(n-2))
+		double firstFactor = 0;
+		if (dataPoints > 2) firstFactor = dataPoints/((dataPoints-1)*(dataPoints-2));
+		
+		////Calculate distSum of |x - avg|^3
+		double distSum = 0;
+		for(int i = a; i < b; i++) {
+			if (i >= 0 && i < data.size() && data.get(i) != null) {
+				distSum += Math.pow(Math.abs(data.get(i) - avg), 3);
+			}
+		}
+		
+		//Divides distSum by stdev^3 to get second factor
+		double secondFactor = distSum/Math.pow(stdev(data, a, b), 3);
+		
+		skewness = firstFactor * secondFactor;
+		
+		return secondFactor;
+		
+	}
+	
+	//Returns actual number of datapoints between two pivots
+	public int numberOfDatapoints (List<Double> data, int a, int b) {
+		
+		int count = 0;
+		for(int i = a; i < b; i++) {
+			
+			if (i >= 0 && i < data.size() && data.get(i) != null) {
+				count++;
+			}
+			
+		}
+		
+		return count;
+		
+	}
+	
+	//Finds the approximate time of when the studied user starts to move
+	//searchRange works best at 10
+	//diffLimit should be 0.6 for head and 0.8 for gsr
+	//stepLength is used to calculate real position in array
+	public int findStartMove(List<Double> data, int searchRange, double diffLimit, int stepLength) throws FileNotFoundException, IOException {
+			
+		for (int i = 5; i < data.size(); i++) {
+			
+			//In this case 12 will equal twelve pair of 5 second units = 60 sek
+			double backward = (new DataHandler()).maxVal(data, i - searchRange, i);
+			double forward = (new DataHandler()).maxVal(data, i, i + searchRange);
+			double diff = forward - backward;
+			
+			//Around 0.3 when still and over 0.8 when starting to move
+			if (diff > diffLimit) {
+				return i*((int) (stepLength/1000));
+			}
+			
+		}
+		
+		return 0;
+		
+	}
+	
+	//Extracts deviation from average and averages that data. Used when finding start of movement
+	//stepLength should be 5000 for both devices
+	public List<Double> extract(List<Double> data, int stepLength) {
+		
+		//List that will contain the data
+		List<Double> extractedData = new ArrayList<Double>();
+		
+		//Gets average
+		double avg = 0;
+		try {
+			avg = average(data, 0, data.size() - 1);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int averageRange = (int) stepLength/2;
+		for (int i = 0; i < data.size(); i+=stepLength) {
+			
+			//Average data from averageRange back and forth relative to pivot point
+			int count = 0;
+			double sum = 0;
+			for (int a = (i - averageRange); a < (i + averageRange); a++) {
+				
+				if (a >= 0 && a < data.size() && data.get(a) != null) {
+					count++;
+					sum += Math.abs(avg-data.get(a)); 
+				}
+				
+			}
+			
+			//Adds extraced info to extracted data
+			extractedData.add(sum/count);
+			
+		}
+		
+		return extractedData;
+		
+	}
+	
+	//Downsamples a given list with values from one sample rate to another
 	public List<Double> downsample(List<Double> list, int from, int to) {
 		
 		List<Double> downsampled = new ArrayList<Double>();
@@ -236,32 +333,22 @@ public List<Double> accelerometerMagnitude(List<String> x, List<String> y, List<
 		int position = 0;
 		
 		while (Math.round(pos) < list.size()) {
-			
 			position = (int) Math.round(pos);
 			double sum = 0;
 			double count = 0;
 			
 			for (int i = 0; i < Math.round(offset); i++) {
-				
 				int newPos = (int) Math.round(position - offset/2 + i);
-				
 				if (newPos >= 0 && list.get(newPos) != null) {
-					
 					sum += list.get(newPos);
 					count++;
-					
 				}
-				
 			}
 			
 			if (count > 0) {
-				
 				downsampled.add((double) (sum/count));
-				
 			} else {
-				
 				downsampled.add(null);
-				
 			}
 			pos+=offset;
 			
@@ -270,5 +357,99 @@ public List<Double> accelerometerMagnitude(List<String> x, List<String> y, List<
 		return downsampled;
 		
 	}
+	
+	//Converts the timestamp to milliseconds and returns the results
+	public int convertTimestamp(String time) {
+		
+		double timeInMilli = 0;
+		
+		String[] timeParts = time.split(":");
+		
+		//Starts at end of 00:00:00, multiplies by 60^(2-2), then reads second part and multiply by 60^(2-1)
+		for (int i = timeParts.length - 1; i >= 0; i--) {
+			timeInMilli += (Double.parseDouble(timeParts[i]) * Math.pow(60, timeParts.length - 1 - i))*1000;
+		}
+		
+		return (int) timeInMilli;
+		
+	}
+	
+	/* End of general data extraction functions */
+	/* --------------------------------------------------------------------- */
+		
+	/* --------------------------------------------------------------------- */
+	/* This section contains data fetch functions */
+	
+	//General function to read all files. Reads file and converts data to correct format
+	//where array position equal to milliseconds
+	public List<Double> dataFetch(File file) throws FileNotFoundException, IOException { 
+		
+		List<String> tempArr = new ArrayList<String>();
+		List<Double> finalArr = new ArrayList<Double>();
+		int sampleRate = 0;
+		int arrayPos = 0;
+		double samplePos = 0;
+		int startRow = 0;
+		
+		//Reads the correct parameters to handle file (BVP)
+		if(file.getName().contains("ACC")){
+			tempArr = (new FileHandler()).getColumn(file, 0);	
+			sampleRate = (int) Double.parseDouble(tempArr.get(1));
+			startRow = 2;
+		}
+		
+		//Reads the correct parameters to handle file (BVP)
+		if(file.getName().contains("BVP")){
+			tempArr = (new FileHandler()).getColumn(file, 0);	
+			sampleRate = (int) Double.parseDouble(tempArr.get(1));
+			startRow = 2;
+		}
+		
+		//Reads the correct parameters to handle file (EDA)
+		if(file.getName().contains("EDA")){
+			tempArr = (new FileHandler()).getColumn(file, 0);	
+			sampleRate = (int) Double.parseDouble(tempArr.get(1));
+			startRow = 3;
+		}
+		
+		//Reads the correct parameters to handle file (HR) heartrate
+		if(file.getName().contains("HR")){
+			tempArr = (new FileHandler()).getColumn(file, 0);
+			sampleRate = (int) Double.parseDouble(tempArr.get(1));
+			startRow = 2;
+		}
+		
+		//Reads the correct parameters to handle file (TEMP) temperature
+		if(file.getName().contains("TEMP")){
+			tempArr = (new FileHandler()).getColumn(file, 0);	
+			//Limit temp to 100 because of 382.18. Sets every elemtent bigger than 100 to element 6
+			//Should be approximately right
+			for (int i = 0; i < tempArr.size(); i++) {
+				if (tempArr.get(i) != null && Double.parseDouble(tempArr.get(i)) > 100) tempArr.set(i, tempArr.get(6));
+			}
+			sampleRate = (int) Double.parseDouble(tempArr.get(1));
+			startRow = 2;
+		}
+		
+		//Converts string to double and to correct format
+		int i = startRow;
+		while (i < tempArr.size()) {
+			if (arrayPos == (int) samplePos) {
+				finalArr.add(Double.parseDouble(tempArr.get(i)));
+				samplePos+=1000.0/sampleRate;
+				i++;
+			} else {
+				finalArr.add(null);
+			}
+			
+			arrayPos++;
+		}
+
+		return finalArr;
+		
+	}
+	
+	/* End of general functions section */
+	/* --------------------------------------------------------------------- */
 
 }
--- src/FileHandler.java
+++ src/FileHandler.java
@@ -2,15 +2,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
 	
 	public List<String> getColumn(File file, int columnNumber) throws FileNotFoundException, IOException {
+		
 		List<String> columnData = new ArrayList<String>();
 		
 		String line, encoding = "UTF-8", delimiter = ","; // Default is .csv
@@ -27,6 +30,242 @@ public class FileHandler {
 				columnData.add(null);
 			}
 		}
+		
+		in.close();
 		return columnData;
+		
     }
+	
+	//Same function as getColumn but it also lets the user to specify how many item to fetch
+	public List<String> getColumn(File file, int columnNumber, int fetchSize) throws FileNotFoundException, IOException {
+		
+		List<String> columnData = new ArrayList<String>();
+		
+		String line, encoding = "UTF-8", delimiter = ","; // Default is .csv
+		if(file.getName().contains(".tsv")){
+			encoding = "UTF-16";
+			delimiter = "\t";
+		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+
+		int count = 0;
+		while ((line = in.readLine()) != null){
+			if (line.split(delimiter).length > columnNumber) {
+				columnData.add(line.split(delimiter)[columnNumber]);
+			} else {
+				columnData.add(null);
+			}
+			count++;
+			if (count > fetchSize) {
+				in.close();
+				return columnData;
+			}
+		}
+		
+		in.close();
+		return columnData;
+		
+    }
+	
+	//Extract data column from headset file
+	//Default start on row 1
+	public List<Double> getRecColumn(File file, int columnNumber) throws FileNotFoundException, IOException { //headset
+		
+		//Fetching time
+		List<String> time = new ArrayList<String>();
+		time = (new FileHandler()).getColumn(file, 3);
+		
+		//Input data in string format
+		List<String> stringData = new ArrayList<String>();
+		stringData = getColumn(file, columnNumber);
+		
+		//List to hold final double data
+		List<Double> data = new ArrayList<Double>();
+		
+		//Using time to put the total magnitute data in the right place. 
+		double tempData;
+		for (int i = 1; i < stringData.size(); i++) {
+			
+			if (Integer.parseInt(time.get(i)) != 0) {
+				
+				for (int j = 0; j < (Integer.parseInt(time.get(i)) - Integer.parseInt(time.get(i-1)) -1); j++) {
+					data.add(null);
+				}
+			}
+			
+			if (stringData.get(i) == null) {
+				
+				data.add(null);
+				
+			} else if (stringData.get(i).length() > 0) {
+				
+				tempData = Double.parseDouble(stringData.get(i).replaceAll(",", "."));
+				
+				data.add(tempData);
+				
+			} 
+
+		}
+		
+		return data;
+		
+	}
+	
+	/* --------------------------------------------------------------------- */
+	/* File functions */
+	
+	//Deletes a file
+	public void deleteFile(String Path, String fileName) throws IOException {
+		
+		File file = new File(Path + fileName);
+		Files.deleteIfExists(file.toPath());
+		
+	}
+	
+	//Exports data list to specificed column in a CSV file with given parameters 
+	public void exportData(List<?> data, String path, String fileName, String columnName, int columnNumber) throws IOException {
+		
+		//Reads in old data to be able to determine how to insert data into the correct column
+		List<String> oldData = new ArrayList<String>();
+		File file = new File(path + fileName);
+		
+		//Creates a new empty file if no exist
+		if (!file.exists()) file.createNewFile();
+		
+		String line, encoding = "UTF-8";
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+
+		//Gets number of seperators, or ,
+		while ((line = in.readLine()) != null){
+			oldData.add(line);
+		}
+		
+		in.close();
+		
+		try {
+		
+			PrintWriter writer = new PrintWriter(file, "UTF-8");
+			
+			boolean firstRow = true;
+			
+			//Writes til the longest list is complete
+			for (int i = 0; i < Math.max(oldData.size(), data.size()); i++) {
+				
+				//For each row 
+				int numberOfColumns = 0;
+				String currentLine = "";
+				String newData = "";
+				
+				//Get old line data
+				if (i >= 0 && i < oldData.size() && oldData.get(i) != null) {
+					currentLine += oldData.get(i);
+				} else {
+					currentLine += "";
+				}
+				
+				//Get new data
+				if (i >= 0 && i < data.size() && data.get(i) != null) {
+					
+					//If first row, write columnName
+					if (firstRow) {
+						newData += columnName;
+						firstRow = false;
+					} else {
+						newData += data.get(i);
+					}
+				} else {
+					newData += "";
+				}
+				
+				//Gets number of columns at given line
+				if (currentLine == "") {
+					numberOfColumns = 0;
+				} else {
+					numberOfColumns = currentLine.length() - currentLine.replace(",", "").length();
+				}
+				
+				//Inserts data into line 
+				String newString = "";
+				if (columnNumber > numberOfColumns) {
+					//If columnNumber is larger than number of existing number, then you just add right amount of ,
+					int seperatorsToAdd = columnNumber - numberOfColumns;
+					newString = currentLine + new String(new char[seperatorsToAdd]).replace("\0", ",") + newData;
+				} else {
+					if (columnNumber == 0) {
+						//Adds data value first and the the rest from old
+						newString += newData;
+						if (currentLine.indexOf(",") != -1) newString += currentLine.substring(currentLine.indexOf(","), currentLine.length());
+					} else if (columnNumber == data.size()) {
+						//Adds up to last , from old and then data value
+						newString += currentLine.substring(i, currentLine.lastIndexOf(","));
+						newString += newData;
+					} else {
+						//Finds index of Nth occurence of , and then insert and then adds the rest
+						//Finds end index of first part
+						String tempString = currentLine;
+						int index = 0;
+						for (int a = 0; a < columnNumber; a++) {
+							int tempLength = tempString.length();
+							tempString = tempString.substring(tempString.indexOf(",") + 1, tempString.length());
+							index += tempLength - tempString.length();
+						}
+						newString += currentLine.substring(0, index);
+						//Adds data
+						newString += newData;
+						//Finds start index of second part
+						int tempLength = tempString.length();
+						tempString = tempString.substring(tempString.indexOf(","), tempString.length());
+						index += tempLength - tempString.length();
+						newString += currentLine.substring(index, currentLine.length());
+						
+					}
+				}
+				
+				//Writes final string to file
+				writer.println(newString);
+			
+			}
+			
+			writer.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public int getColumnNumber(String columnName, String headsetPath) throws IOException {
+		
+		//For Headset
+		File headsetFile = new File(headsetPath);
+		
+		String[] line;
+		String encoding = "UTF-8", delimiter = ","; // Default is .csv
+		if(headsetFile.getName().contains(".tsv")){
+			encoding = "UTF-16";
+			delimiter = "\t";
+		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(headsetFile), encoding));
+
+		line = in.readLine().split(delimiter);
+		
+		int index = 0;
+		for (String name : line) {
+			
+			//Acc X, Y and Z is unnecessary
+			if (name.equals(columnName)) {
+				return index;
+			}
+			
+			index++;
+
+		}		
+		
+		in.close();
+		
+		return 0;
+		
+	}
+	
 }
